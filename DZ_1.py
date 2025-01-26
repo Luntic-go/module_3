569107c1eac2dda1438d121fee6f879b5b0d5967
Luntic-go <eliza.nikaa@mail.ru>
2025-01-26 09:59:26 +0300

    Update DZ_1.py

--- DZ_1.py
+++ DZ_1.py
@@ -1,23 +1,32 @@
 calls = 0
+
+
 def count_calls():
     global calls
-    calls+=1
+    calls += 1
+
 
 def string_info(string):
     count_calls()
-    return(len(string), string.upper(), string.lower())
+    return (len(string), string.upper(), string.lower())
+
 
 def is_contains(string, list_to_search):
     count_calls()
     for i in range(0, len(list_to_search)):
         str_is_list = False
         if string.upper() in list_to_search[i].upper():
-            return True
+            str_is_list = True
         else:
-            continue
+            if str_is_list == True:
+                continue
+            else:
+                str_is_list = False
+    return str_is_list
+
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBan
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBan
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
